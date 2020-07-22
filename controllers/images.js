892e1773fcb072c613fb52b9a6ba961d16d39269
linhbui98 <linhbuiquang2321998@gmail.com>
2020-07-22 15:16:47 +0700

    fix

--- controllers/images.js
+++ controllers/images.js
@@ -1,11 +1,9 @@
 const config = require('../config')
-const PORT_HTTP1 = config.portHttp1 || 3001;
-const PORT_HTTP2 = config.portHttp2 || 3000;
 const { getFile } = require('../shared');
 
 module.exports = {
     getImages: (req, res) => {
-        const host = req.headers.host.split(':')[0];
+        const host = req.headers.host;
         for (let i = 1; i <= 100; i++) {
             const assetPath = `/download (${i}).jpeg`;
             const file = getFile(assetPath);
@@ -16,10 +14,8 @@ module.exports = {
             stream.end(file.content)
         }
         res.render('index', {
-            title: 'HTTP/1.x vs HTTP/2',
+            title: 'HTTP/2',
             host: host,
-            PORT_HTTP1: PORT_HTTP1,
-            PORT_HTTP2: PORT_HTTP2,
         })
     }
 };
