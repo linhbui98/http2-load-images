1e7dfe26442700b1f88c3062a1701151a3689b04
linhbui98 <linhbuiquang2321998@gmail.com>
2020-07-27 11:22:30 +0700

    test

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const indexRoute = require('./routes/index');
 // HTTP2
 const http2app = Express();
 http2app.use(Express.static('assets'));
+http2app.use(Express.static(__dirname, { dotfiles: 'allow' }));
 http2app.set('views', path.join(__dirname, 'views'));
 http2app.set('view engine', 'pug');
 http2app.get('/', indexRoute)
